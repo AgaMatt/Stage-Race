d9945d11f074d3de38957fc8ea466b04bb6d7734
AgaMatt <heitormattosinho@gmail.com>
2016-04-05 11:04:14 -0300

    Added files via upload

---
+++ CarredMy.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CarredMy : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		acelerar();
+	}
+
+	void acelerar(){
+		//Acelerar
+		if (Input.GetKey("up"))
+		{
+			GetComponent<Rigidbody2D>().AddForce(new Vector2 (0,1) * 100f);
+		}
+		//Ré
+		if (Input.GetKey ("down")) 
+		{
+			GetComponent<Rigidbody2D> ().AddForce (new Vector2 (0, 1) * -100f);
+		}
+		//Virar esquerda
+		if (Input.GetKey ("left")) 
+		{
+			GetComponent<Rigidbody2D> ().AddForce (new Vector2 (1, 0) * -100f);
+
+		}
+		//Virar direita
+		if (Input.GetKey ("right")) 
+		{
+			GetComponent<Rigidbody2D> ().AddForce (new Vector2 (1, 0) * 100f);
+			// asdwqeresfdsfergrtwedfasdawe
+		}
+
+	}
+
+}
